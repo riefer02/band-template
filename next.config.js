3b3d8962db805237cce911e9351947426ded4336
riefer02 <andrew.riefenstahl@gmail.com>
2021-12-31 16:21:32 -0600

    update packages

--- next.config.js
+++ next.config.js
@@ -3,6 +3,5 @@ const { withPlaiceholder } = require('@plaiceholder/next');
 module.exports = withPlaiceholder({
   images: {
     domains: ['lewdapi.com'],
-    // formats: ['image/avif', 'image/webp'],
   },
 });
